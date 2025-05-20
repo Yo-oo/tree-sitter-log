90e2550225819c46d5dd346992603b9247a3c47d
yo <uniball041816@gmail.com>
2025-05-20 10:12:53 +0800

    feat: add neovim highlights.scm

---
+++ queries/neovim_highlights.scm
@@ -0,0 +1,26 @@
+(info) @comment.note
+(warn) @comment.warning
+(error) @comment.error
+(trace) @comment
+(debug) @comment.todo
+(year_month_day) @markup.strong
+(time) @markup.italic
+(string_literal) @string
+(number) @number
+(constant) @constant
+[
+(url)
+(file_path)
+] @markup.link
+[
+(ipv4)
+(ipv6)
+(mac)
+(uuid)
+(md5)
+(sha1)
+(sha224)
+(sha256)
+(sha384)
+(sha512)
+] @symbol
